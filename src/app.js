b0ff6118075b4da28b57bd73fda04e535b759c8c
LucyMacko <lucia.mackova87@gmail.com>
2023-03-28 16:45:25 +0000

    creating

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = [
+  "The dog",
+  "My grandma",
+  "His turtle",
+  "My bird",
+  "The dragon",
+  "A unicorn"
+];
+let action = ["ate", "dragged", "broke", "chewed", "dug", "stomped"];
+let what = [
+  "my homework",
+  "the keys",
+  "the car",
+  "the screen",
+  "my bills",
+  "the report"
+];
+let when = [
+  "before the class",
+  "after it was done",
+  "when I finished",
+  "during my lunch",
+  "while I was taking a nap",
+  "while I was cooking"
+];
